38942b03d741e0922daf341845cbb9759dfb4173
nikhilrp <nikhilrp@stanford.edu>
2014-08-12 14:25:19 -0700

    added travis files and basic structure for tests

---
+++ tests/command-line-args-test.h
@@ -0,0 +1,14 @@
+#ifndef COMMAND_LINE_ARGS_TEST_H
+#define COMMAND_LINE_ARGS_TEST_H
+
+#include "test.h"
+
+TEST(CommandLineArgsTest, SetArgs) {
+
+}
+
+TEST(CommandLineArgsTest, LoggingFlagsArg) {
+
+}
+
+#endif // COMMAND_LINE_ARGS_TEST_H
---
+++ tests/main.cpp
@@ -0,0 +1,25 @@
+/*****************************************************************************
+  main.cpp
+
+  (c) 2014 - Nikhil R Podduturi
+  J. Michael Cherry Lab, Department of Genetics, Stanford University School of Medicine
+
+  Licensed under the GNU General Public License 2.0 license.
+******************************************************************************/
+
+// gtest based unit tests
+#include "test.h"
+
+#include "command-line-args-test.h"
+
+TIMED_SCOPE(testTimer, "IDR Unit Tests");
+
+int main() {
+    testing::InitGoogleTest(&argc, argv);
+    _INIT_SYSLOG(kSysLogIdent, 0, 0);
+
+    reconfigureLoggersForTest();
+    std::cout << "Logs for test are written in [" << logfile << "]" << std::endl;
+
+    return ::testing::UnitTest::GetInstance()->Run();
+}
