286714789d448a7a0e452216ff95208c8910e00e
nikhilrp <nikhilrp@stanford.edu>
2014-10-07 13:23:49 -0700

    got rid of unnecessary docs

---
+++ include/idr.h
@@ -0,0 +1,290 @@
+/*************************************************************************************
+ idr.h
+
+ (c) 2014 - Nikhil R Podduturi
+ J. Michael Cherry Lab, Department of Genetics, Stanford University School of Medicine
+
+ Licensed under the GNU General Public License 2.0 license.
+**************************************************************************************/
+#ifndef IDR_H
+#define IDR_H
+
+#include <math.h>
+#include <float.h>
+#include <numeric>
+
+template <typename V>
+void calculate_quantiles(V& x_cdf, V& y_cdf, V rho, V& density)
+{
+    for(int i=0; i<x_cdf.size(); ++i)
+    {
+        double a = pow(normcdfinv(x_cdf[i]), 2) + pow(normcdfinv(y_cdf[i]), 2);
+        double b = normcdfinv(x_cdf[i]) * normcdfinv(y_cdf[i]);
+        density[i] = exp( -log(1 - pow(rho, 2)) / 2 - rho / (2 * (1 - pow(rho, 2))) * (rho*a-2*b) );
+    }
+}
+
+template <typename V>
+double cost_function(V& x_cdf, V& y_cdf, V& ez, V rho)
+{
+    typedef typename V::value_type ValueType;
+
+    vector<ValueType> density(x_cdf.size());
+    vector<ValueType> new_density(x_cdf.size());
+
+    calculate_quantiles(x_cdf, y_cdf, rho, density);
+
+    ValueType cop_den = 0.0;
+    for(int i=0; i<density.size(); ++i)
+    {
+        cop_den = cop_den + (ez[i] * log(density[i]));
+    }
+    return -cop_den;
+}
+
+void calculate_quantiles()
+{
+
+}
+
+void sum_likelihood()
+{
+
+}
+
+void get_ez()
+{
+
+}
+
+template <typename V>
+double maximum_likelihood(V& x_cdf, V& y_cdf, V& ez)
+{
+    typedef typename V::value_type ValueType;
+
+    double ax = -0.998;
+    double bx = 0.998;
+    double tol = 0.00001;
+
+    /*  c is the squared inverse of the golden ratio */
+    const double c = (3. - sqrt(5.)) * .5;
+
+    /* Local variables */
+    double a, b, d, e, p, q, r, u, v, w, x;
+    double t2, fu, fv, fw, fx, xm, eps, tol1, tol3;
+
+    /*  eps is approximately the square root of the relative machine precision. */
+    eps = DBL_EPSILON;
+    tol1 = eps + 1.;/* the smallest 1.000... > 1 */
+    eps = sqrt(eps);
+
+    a = ax;
+    b = bx;
+    v = a + c * (b - a);
+    w = v;
+    x = v;
+
+    d = 0.;/* -Wall */
+    e = 0.;
+    fx = cost_function(x_cdf, y_cdf, ez, x);
+    fv = fx;
+    fw = fx;
+    tol3 = tol / 3.;
+
+    for(;;)
+    {
+        xm = (a + b) * .5;
+        tol1 = eps * fabs(x) + tol3;
+        t2 = tol1 * 2.;
+
+        /* check stopping criterion */
+        if (fabs(x - xm) <= t2 - (b - a) * .5) break;
+        p = 0.;
+        q = 0.;
+        r = 0.;
+        if (fabs(e) > tol1)
+        { /* fit parabola */
+            r = (x - w) * (fx - fv);
+            q = (x - v) * (fx - fw);
+            p = (x - v) * q - (x - w) * r;
+            q = (q - r) * 2.;
+            if (q > 0.) p = -p; else q = -q;
+            r = e;
+            e = d;
+        }
+
+        if (fabs(p) >= fabs(q * .5 * r) ||
+            p <= q * (a - x) || p >= q * (b - x))
+        { /* a golden-section step */
+            if (x < xm) e = b - x; else e = a - x;
+            d = c * e;
+        }
+        else
+        { /* a parabolic-interpolation step */
+            d = p / q;
+            u = x + d;
+            /* f must not be evaluated too close to ax or bx */
+            if (u - a < t2 || b - u < t2)
+            {
+                d = tol1;
+                if (x >= xm) d = -d;
+            }
+        }
+
+        /* f must not be evaluated too close to x */
+        if (fabs(d) >= tol1)
+            u = x + d;
+        else if (d > 0.)
+            u = x + tol1;
+        else
+            u = x - tol1;
+
+        fu = cost_function(x_cdf, y_cdf, ez, u);
+
+        /*  update  a, b, v, w, and x */
+        if (fu <= fx)
+        {
+            if (u < x) b = x; else a = x;
+            v = w;    w = x;   x = u;
+            fv = fw; fw = fx; fx = fu;
+        }
+        else
+        {
+            if (u < x) a = u; else b = u;
+            if (fu <= fw || w == x)
+            {
+                v = w; fv = fw;
+                w = u; fw = fu;
+            }
+            else if (fu <= fv || v == x || v == w)
+            {
+                v = u; fv = fu;
+            }
+        }
+    }
+    return x;
+}
+
+template <typename V>
+double gaussian_loglikelihood(V& x1_pdf, V& x2_pdf, V& x1_cdf, V& x2_cdf,
+        V& y1_pdf, V& y2_pdf, V& y1_cdf, V& y2_cdf, double p, double rho)
+{
+    typedef typename V::value_type ValueType;
+
+    vector<ValueType> density_c1( x1_pdf.size() );
+    vector<ValueType> likelihood_vec( x1_pdf.size() );
+
+    // rewrite this
+    calculate_quantiles();
+    sum_likelihood();
+
+    double l0 = accumulate(likelihood_vec.begin(), likelihood_vec.end(), 0.0);
+
+    density_c1.clear();
+    density_c1.shrink_to_fit();
+
+    likelihood_vec.clear();
+    likelihood_vec.shrink_to_fit();
+
+    return l0;
+}
+
+// TODO: duplication of code, should be refactored
+template <typename V>
+void estep_gaussian(V& x1_pdf, V& x2_pdf, V& x1_cdf, V& x2_cdf,
+        V& y1_pdf, V& y2_pdf, V& y1_cdf, V& y2_cdf, V& ez, double p, double rho)
+{
+    typedef typename V::value_type ValueType;
+
+    vector<ValueType> density_c1( x1_pdf.size() );
+
+    // Rewrite those GPU kernals
+    calculate_quantiles();
+    get_ez();
+
+    density_c1.clear();
+    density_c1.shrink_to_fit();
+}
+
+template <typename V, typename T, typename P, typename R>
+void mstep_gaussian(V& x, V& y, T& breaks, P *p0,  R *rho,
+        T& x1_pdf, T& x2_pdf, T& x1_cdf, T& x2_cdf, T& y1_pdf,
+        T& y2_pdf, T& y1_cdf, T& y2_cdf, T& ez)
+{
+
+    estimate_marginals(x, breaks, x1_pdf, x2_pdf, x1_cdf, x2_cdf, ez, *p0);
+    estimate_marginals(y, breaks, y1_pdf, y2_pdf, y1_cdf, y2_cdf, ez, *p0);
+
+    *rho = maximum_likelihood(x1_cdf, y1_cdf, ez);
+
+    double sum_ez = accumulate(ez.begin(), ez.end(), 0.0) / x.size();
+    *p0 = sum_ez/(double)x.size();
+}
+
+template <typename V, typename T>
+void em_gaussian(V& x, V& y, T& idrLocal)
+{
+    typedef typename V::value_type ValueType;
+
+    vector<ValueType> ez( x.size() );
+    vector<ValueType> breaks(51);
+
+    int mid = round((float) x.size()/2);
+
+    fill(ez.begin(), ez.begin()+mid, 0.9);
+    fill(ez.begin()+mid, ez.end(), 0.1);
+
+    float bin_width = (float)(x.size()-1)/50;
+
+    // Constants chosen by Li and Anshul
+    double p0 = 0.5;
+    double rho = 0.0;
+    double eps = 0.01;
+
+    vector<double> likelihood;
+    breaks[0] = (double)1-bin_width/100;
+    iota(breaks.begin(), breaks.end(), (double)(x.size()-1+bin_width/50)/50);
+
+    vector<double> x1_pdf(x.size()), x2_pdf(x.size()), x1_cdf(x.size()), x2_cdf(x.size());
+    vector<double> y1_pdf(y.size()), y2_pdf(y.size()), y1_cdf(y.size()), y2_cdf(y.size());
+
+    mstep_gaussian(x, y, breaks, &p0, &rho, x1_pdf, x2_pdf,
+            x1_cdf, x2_cdf, y1_pdf, y2_pdf, y1_cdf, y2_cdf, ez);
+
+    double li = gaussian_loglikelihood(x1_pdf, x2_pdf, x1_cdf, x2_cdf,
+            y1_pdf, y2_pdf, y1_cdf, y2_cdf, p0, rho);
+
+    likelihood.push_back(li);
+
+    bool flag = true;
+    int i = 1;
+
+    //can do better. Jus replicating IDR R style coding
+    int iter_counter = 1;
+    while(flag)
+    {
+        estep_gaussian(x1_pdf, x2_pdf, x1_cdf, x2_cdf,
+                 y1_pdf, y2_pdf, y1_cdf, y2_cdf, ez, p0, rho);
+
+        mstep_gaussian(x, y, breaks, &p0, &rho, x1_pdf, x2_pdf,
+                x1_cdf, x2_cdf, y1_pdf, y2_pdf, y1_cdf, y2_cdf, ez);
+
+        double l = gaussian_loglikelihood(x1_pdf, x2_pdf, x1_cdf, x2_cdf,
+                y1_pdf, y2_pdf, y1_cdf, y2_cdf, p0, rho);
+        likelihood.push_back(l);
+
+        if (i > 1)
+        {
+            double a_cri = likelihood[i-2] + (likelihood[i-1] - likelihood[i-2])/(1-(likelihood[i]-likelihood[i-1])/(likelihood[i-1]-likelihood[i-2]));
+            if ( abs(a_cri-likelihood[i]) <= eps )
+            {
+                flag = false;
+            }
+        }
+        i++;
+        iter_counter++;
+    }
+}
+
+#endif
+
--- src/idr.cpp
+++ src/idr.cpp
@@ -12,6 +12,7 @@
 
 #include <processPeaks.h>
 #include <ranker.h>
+#include <idr.h>
 
 using namespace std;
 
@@ -37,176 +38,171 @@ struct overlap {
 
 int main(int argc, char* argv[])
 {
-	bool showHelp = false;
+    bool showHelp = false;
 
-	string bedAFile;
-	string bedBFile;
-	string genomeFile;
-	string rankingMeasure = "signalValue";
+    string bedAFile;
+    string bedBFile;
+    string genomeFile;
+    string rankingMeasure = "signalValue";
 
-	bool haveBedA = false;
-	bool haveBedB = false;
-	bool haveGenome = false;
+    bool haveBedA = false;
+    bool haveBedB = false;
+    bool haveGenome = false;
 
-	if(argc <= 1) showHelp = true;
+    if(argc <= 1) showHelp = true;
 
-	for(int i = 1; i < argc; i++) {
-		int parameterLength = (int)strlen(argv[i]);
+    for(int i = 1; i < argc; i++) {
+        int parameterLength = (int)strlen(argv[i]);
+        if((PARAMETER_CHECK("-h", 2, parameterLength)) ||
+            (PARAMETER_CHECK("--help", 5, parameterLength))) {
+                showHelp = true;
+        }
+    }
 
-		if((PARAMETER_CHECK("-h", 2, parameterLength)) ||
-		(PARAMETER_CHECK("--help", 5, parameterLength))) {
-			showHelp = true;
-		}
-	}
+    if(showHelp) ShowHelp();
 
-	if(showHelp) ShowHelp();
+    for(int i = 1; i < argc; i++) {
+        int parameterLength = (int)strlen(argv[i]);
+        if(PARAMETER_CHECK("-a", 2, parameterLength)) {
+            if ((i+1) < argc) {
+                haveBedA = true;
+                bedAFile = argv[i + 1];
+                i++;
+            }
+        }
+        else if(PARAMETER_CHECK("-b", 2, parameterLength)) {
+            if ((i+1) < argc) {
+                haveBedB = true;
+                bedBFile = argv[i + 1];
+                i++;
+            }
+        }
+        else if(PARAMETER_CHECK("-g", 2, parameterLength)) {
+            if ((i+1) < argc) {
+                haveGenome = true;
+                genomeFile = argv[i + 1];
+                i++;
+            }
+        }
+        else if(PARAMETER_CHECK("-rank", 5, parameterLength)) {
+            if((i+1) < argc) {
+                rankingMeasure = argv[i + 1];
+                i++;
+            }
+        }
+        else {
+            fprintf(stderr, "*****ERROR: Unrecognized parameter: %s *****\n\n", argv[i]);
+            showHelp = true;
+        }
+    }
+    // make sure we have both input files
+    if (!haveBedA || !haveBedB || !haveGenome) {
+        fprintf(stderr, "*****ERROR: Need -a and -b files. *****");
+        showHelp = true;
+    }
 
-	for(int i = 1; i < argc; i++) {
+    if (!showHelp) {
+        ProcessPeaks *bc = new ProcessPeaks(bedAFile, bedBFile, genomeFile);
+        vector<double> merge_A, merge_B, unmatched_merge_A, unmatched_merge_B;
+        vector<unsigned int> tracker;
+        vector<overlap> overlaps;
 
-		int parameterLength = (int)strlen(argv[i]);
-		if(PARAMETER_CHECK("-a", 2, parameterLength)) {
-			if ((i+1) < argc) {
-				haveBedA = true;
-				bedAFile = argv[i + 1];
-				i++;
-			}
-		}
-		else if(PARAMETER_CHECK("-b", 2, parameterLength)) {
-			if ((i+1) < argc) {
-				haveBedB = true;
-				bedBFile = argv[i + 1];
-				i++;
-			}
-		}
-		else if(PARAMETER_CHECK("-g", 2, parameterLength)) {
-			if ((i+1) < argc) {
-				haveGenome = true;
-				genomeFile = argv[i + 1];
-				i++;
-			}
-		}
-		else if(PARAMETER_CHECK("-rank", 5, parameterLength)) {
-			if((i+1) < argc) {
-				rankingMeasure = argv[i + 1];
-				i++;
-			}
-		}
-		else {
-			fprintf(stderr, "*****ERROR: Unrecognized parameter: %s *****\n\n", argv[i]);
-			showHelp = true;
-		}
-	}
-	// make sure we have both input files
-	if (!haveBedA || !haveBedB || !haveGenome) {
-		fprintf(stderr, "*****ERROR: Need -a and -b files. *****");
-		showHelp = true;
-	}
+        unsigned int start = 0;
+        for (size_t i = 0; i < bc->_peakA->bedList.size(); ++i) {
+            if (rankingMeasure == "signalValue") {
+                merge_A.push_back(bc->_peakA->bedList[i].signalValue);
+            }
+            else if(rankingMeasure == "pValue") {
+                merge_A.push_back(bc->_peakA->bedList[i].pValue);
+            }
+            else if(rankingMeasure == "qValue") {
+                merge_A.push_back(bc->_peakA->bedList[i].qValue);
+            }
+            merge_B.push_back(0);
+            while (start < bc->overlap_index_A[i]) {
+                tracker.push_back( bc->overlap_index_B[start] );
+                double bSigVal = 0.0;
+                if (rankingMeasure == "signalValue") {
+                    bSigVal = bc->_peakB->bedList[ bc->overlap_index_B[start] ].signalValue;
+                }
+                else if (rankingMeasure == "pValue") {
+                    bSigVal = bc->_peakB->bedList[ bc->overlap_index_B[start] ].pValue;
+                }
+                else if (rankingMeasure == "qValue") {
+                    bSigVal = bc->_peakB->bedList[ bc->overlap_index_B[start] ].qValue;
+                }
 
-	if (!showHelp) {
-		ProcessPeaks *bc = new ProcessPeaks(bedAFile, bedBFile, genomeFile);
-		vector<double> merge_A, merge_B, unmatched_merge_A, unmatched_merge_B;
-		vector<unsigned int> tracker;
-		vector<overlap> overlaps;
+                overlap o;
+                string chr(bc->_peakA->bedList[i].chrom);
+                o.chr = chr;
+                o.start1 = bc->_peakA->bedList[i].start;
+                o.end1 = bc->_peakA->bedList[i].end;
+                o.rankingMeasure1 = merge_A[i];
+                o.start2 = bc->_peakB->bedList[ bc->overlap_index_B[start] ].start;
+                o.end2 = bc->_peakB->bedList[ bc->overlap_index_B[start] ].end;
 
-		unsigned int start = 0;
-		for (size_t i = 0; i < bc->_peakA->bedList.size(); ++i) {
-			if (rankingMeasure == "signalValue") {
-				merge_A.push_back(bc->_peakA->bedList[i].signalValue);
-			}
-			else if(rankingMeasure == "pValue") {
-				merge_A.push_back(bc->_peakA->bedList[i].pValue);
-			}
-			else if(rankingMeasure == "qValue") {
-				merge_A.push_back(bc->_peakA->bedList[i].qValue);
-			}
+                if (merge_B[i] == 0) {
+                    merge_B[i] = bSigVal;
+                    o.rankingMeasure2 = merge_B[i];
+                    overlaps.push_back(o);
+                }
+                else {
+                    merge_B[i] = double (merge_B[i] + bSigVal)/2.0;
+                    o.rankingMeasure2 = merge_B[i];
+                    overlaps.pop_back();
+                    overlaps.push_back(o);
+                }
+                ++start;
+            }
+        }
+        sort(tracker.begin(), tracker.end());
+        for (unsigned int i = 0; i < bc->_peakB->bedList.size(); ++i) {
+            if(find(tracker.begin(), tracker.end(), i) == tracker.end()) {
+                double bSigVal = 0.0;
+                if (rankingMeasure == "signalValue") {
+                    bSigVal = bc->_peakB->bedList[i].signalValue;
+                }
+                else if (rankingMeasure == "pValue") {
+                    bSigVal = bc->_peakB->bedList[i].pValue;
+                }
+                else if (rankingMeasure == "qValue") {
+                    bSigVal = bc->_peakB->bedList[i].qValue;
+                }
+                merge_A.push_back( 0 );
+                merge_B.push_back( bSigVal );
+            }
+        }
 
-			merge_B.push_back(0);
-			while (start < bc->overlap_index_A[i]) {
+        for (unsigned int i=0; i < merge_A.size(); ++i) {
+            if ( merge_A[i] != 0 && merge_B[i] != 0 ) {
+                unmatched_merge_A.push_back( -merge_A[i] );
+                unmatched_merge_B.push_back( -merge_B[i] );
+            }
+        }
 
-				tracker.push_back( bc->overlap_index_B[start] );
-				double bSigVal = 0.0;
+        fprintf(stderr, "Number of overlaps after removing duplicates - %lu\n", unmatched_merge_A.size());
+        vector<int> ranks_A, ranks_B;
+        rank_vec(unmatched_merge_A, ranks_A, "default");
+        rank_vec(unmatched_merge_B, ranks_B, "default");
 
-				if (rankingMeasure == "signalValue") {
-					bSigVal = bc->_peakB->bedList[ bc->overlap_index_B[start] ].signalValue;
-				}
-				else if (rankingMeasure == "pValue") {
-					bSigVal = bc->_peakB->bedList[ bc->overlap_index_B[start] ].pValue;
-				}
-				else if (rankingMeasure == "qValue") {
-					bSigVal = bc->_peakB->bedList[ bc->overlap_index_B[start] ].qValue;
-				}
-
-				overlap o;
-				string chr(bc->_peakA->bedList[i].chrom);
-				o.chr = chr;
-				o.start1 = bc->_peakA->bedList[i].start;
-				o.end1 = bc->_peakA->bedList[i].end;
-				o.rankingMeasure1 = merge_A[i];
-				o.start2 = bc->_peakB->bedList[ bc->overlap_index_B[start] ].start;
-				o.end2 = bc->_peakB->bedList[ bc->overlap_index_B[start] ].end;
-
-				if (merge_B[i] == 0) {
-					merge_B[i] = bSigVal;
-					o.rankingMeasure2 = merge_B[i];
-					overlaps.push_back(o);
-				}
-				else {
-					merge_B[i] = double (merge_B[i] + bSigVal)/2.0;
-					o.rankingMeasure2 = merge_B[i];
-					overlaps.pop_back();
-					overlaps.push_back(o);
-				}
-				++start;
-			}
-		}
-
-		sort(tracker.begin(), tracker.end());
-		for (unsigned int i = 0; i < bc->_peakB->bedList.size(); ++i) {
-			if(find(tracker.begin(), tracker.end(), i) == tracker.end()) {
-				double bSigVal = 0.0;
-				if (rankingMeasure == "signalValue") {
-					bSigVal = bc->_peakB->bedList[i].signalValue;
-				}
-				else if (rankingMeasure == "pValue") {
-					bSigVal = bc->_peakB->bedList[i].pValue;
-				}
-				else if (rankingMeasure == "qValue") {
-					bSigVal = bc->_peakB->bedList[i].qValue;
-				}
-				merge_A.push_back( 0 );
-				merge_B.push_back( bSigVal );
-			}
-		}
-
-		for (unsigned int i=0; i < merge_A.size(); ++i) {
-			if ( merge_A[i] != 0 && merge_B[i] != 0 ) {
-				unmatched_merge_A.push_back( -merge_A[i] );
-				unmatched_merge_B.push_back( -merge_B[i] );
-			}
-		}
-
-		fprintf(stderr, "Number of overlaps after removing duplicates - %lu\n", unmatched_merge_A.size());
-
-		vector<int> ranks_A, ranks_B;
-		rank_vec(unmatched_merge_A, ranks_A, "default");
-		rank_vec(unmatched_merge_B, ranks_B, "default");
-
-		return 0;
-	}
-	else {
-		ShowHelp();
-	}
-	return 0;
+        vector<double> idrLocal( ranks_A.size() );
+        em_gaussian(ranks_A, ranks_B, idrLocal);
+        return 0;
+    }
+    else {
+        ShowHelp();
+    }
+    return 0;
 }
 
 void ShowHelp(void) {
-	fprintf(stderr, "\n");
-	fprintf(stderr, "Program: IDR (Irreproducible Discovery Rate)\n");
-	fprintf(stderr, "Version: %s\n", PACKAGE_VERSION);
-	fprintf(stderr, "Contact: Nikhil R Podduturi <nikhilrp@stanford.edu>\n\n");
-	fprintf(stderr, "Usage:   idr [options] -a <bed> -b <bed> -g <bed>\n\n");
-	fprintf(stderr, "Options:\n\n");
-	fprintf(stderr, "	-idr		IDR cutoff (default: 0.025)\n\n");
-	fprintf(stderr, "	-rank		Type of ranking measure (default: signal.value)\n\n");
+    fprintf(stderr, "\n");
+    fprintf(stderr, "Program: IDR (Irreproducible Discovery Rate)\n");
+    fprintf(stderr, "Version: %s\n", PACKAGE_VERSION);
+    fprintf(stderr, "Contact: Nikhil R Podduturi <nikhilrp@stanford.edu>\n\n");
+    fprintf(stderr, "Usage:   idr [options] -a <bed> -b <bed> -g <bed>\n\n");
+    fprintf(stderr, "Options:\n\n");
+    fprintf(stderr, "   -idr        IDR cutoff (default: 0.025)\n\n");
+    fprintf(stderr, "   -rank       Type of ranking measure (default: signal.value)\n\n");
     exit(1);
 }
