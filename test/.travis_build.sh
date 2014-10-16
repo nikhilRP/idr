78853e7d5efaf8850dbbc3b1c28f5c5f251b8155
nikhilRP <nikhilrp@stanford.edu>
2014-10-15 17:27:09 -0700

    Fixed tests

--- test/.travis_build.sh
+++ test/.travis_build.sh
@@ -1,3 +1,8 @@
-echo "Compiling unit tests..."
-make -C main.cpp config=release
-echo "Done compiling unit tests :)"
+echo "Compiling application..."
+cd ..
+mkdir bin
+cd bin
+cmake ../
+make
+./idr -a ../test/data/peak1 -b ../test/data/peak2 -g ../test/data/genome_table.txt
+echo "Done compiling application :)"
